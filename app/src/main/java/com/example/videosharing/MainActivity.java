e1fd07c713f0f4fdb75e433ad47dff75e38a0de6
DazedDeer <coolsheep22@gmail.com>
2023-10-14 21:02:46 +1300

    Added dependencies to the gradle files.
    Worked on the main activity xml

--- app/src/main/java/com/example/videosharing/MainActivity.java
+++ app/src/main/java/com/example/videosharing/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.videosharing;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.SignInClient;
+
 public class MainActivity extends AppCompatActivity {
 
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+    private ActivityResultLauncher<IntentSenderRequest> signInRequestLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
