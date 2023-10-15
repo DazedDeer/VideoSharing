c67052ebe0ac1b8d24b6c0790ee3b61885c6b77a
DazedDeer <coolsheep22@gmail.com>
2023-10-15 22:14:07 +1300

    Completed login redirecting.
    Created Youtube Player Activity

--- app/src/main/java/com/example/videosharing/Login.java
+++ app/src/main/java/com/example/videosharing/Login.java
@@ -1,106 +1,97 @@
 package com.example.videosharing;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.IntentSenderRequest;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import com.google.android.gms.auth.api.identity.BeginSignInRequest;
-import com.google.android.gms.auth.api.identity.BeginSignInResult;
-import com.google.android.gms.auth.api.identity.Identity;
-import com.google.android.gms.auth.api.identity.SignInClient;
-import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class Login extends AppCompatActivity {
 
-    private SignInClient oneTapClient;
-    private BeginSignInRequest signInRequest;
-    private ActivityResultLauncher<IntentSenderRequest> signInRequestLauncher;
+    GoogleSignInAccount account;
+    GoogleSignInClient mGoogleSignInClient;
+
+    int RC_SIGN_IN = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        oneTapClient = Identity.getSignInClient(this);
-        signInRequest = BeginSignInRequest.builder().setGoogleIdTokenRequestOptions(
-                        BeginSignInRequest.GoogleIdTokenRequestOptions.builder().setSupported(true)
-                                // Your server's client ID, not your Android client ID.
-                                .setServerClientId(getString(R.string.web_client_id))
-                                // Only show accounts previously used to sign in.
-                                .setFilterByAuthorizedAccounts(false)
-                                .build()
-                )
-                .setAutoSelectEnabled(true)
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
                 .build();
 
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if(account != null) {
+            goToMain();
+        }
+
+        // Set the dimensions of the sign-in button.
         SignInButton signInButton = findViewById(R.id.signIn);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signInOnClick(view);
-            }
-        });
 
-        signInRequestLauncher =
-                registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(),
-                        new ActivityResultCallback<ActivityResult>() {
-                            @Override
-                            public void onActivityResult(ActivityResult result) {
-                                if(result.getResultCode() == Activity.RESULT_OK){
-                                    // sign in successful
-                                    try {
-                                        SignInCredential credential = oneTapClient
-                                                .getSignInCredentialFromIntent(result.getData());
-                                        String idToken = credential.getGoogleIdToken();
-                                        if (idToken != null) {
-                                            // Got an ID token from Google to use for authentication
-                                            //Intent intent = new Intent(
-                                            //  getApplicationContext(),
-                                            //  YouTubePlayerActivtiy.class);
-                                            //intent.putExtra("CREDENTIAL", credential);
-                                            // startActivity(intent);
-                                        }
-                                    } catch (ApiException e) {
-                                        Log.d("API Exception", "issue with sign in");
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }
-                        });
+        signInButton.setOnClickListener(this::onClick);
     }
 
-    public void signInOnClick(View view) {
-        oneTapClient.beginSignIn(signInRequest)
-                .addOnSuccessListener(this, new OnSuccessListener<BeginSignInResult>() {
-                    @Override
-                    public void onSuccess(BeginSignInResult beginSignInResult) {
-                        IntentSenderRequest intentSenderRequest =
-                                new IntentSenderRequest.Builder(beginSignInResult.getPendingIntent().getIntentSender())
-                                        .build();
-                        signInRequestLauncher.launch(intentSenderRequest);
-                    }
-                })
-                .addOnFailureListener(this, new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("FAILURE", "sign in failed!");
-                        e.printStackTrace();
-                    }
-                });
+    private void goToMain() {
+        Intent main = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(main);
     }
 
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.signIn) {
+                signIn();
+            // ...
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Warning", "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+        }
+    }
 }
--- app/src/main/java/com/example/videosharing/MainActivity.java
+++ app/src/main/java/com/example/videosharing/MainActivity.java
@@ -13,12 +13,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.identity.BeginSignInResult;
 import com.google.android.gms.auth.api.identity.Identity;
 import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -26,81 +30,36 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SignInClient oneTapClient;
-    private BeginSignInRequest signInRequest;
-    private ActivityResultLauncher<IntentSenderRequest> signInRequestLauncher;
+    TextView welcome;
+    Button watch_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        oneTapClient = Identity.getSignInClient(this);
-        signInRequest = BeginSignInRequest.builder().setGoogleIdTokenRequestOptions(
-                        BeginSignInRequest.GoogleIdTokenRequestOptions.builder().setSupported(true)
-                                // Your server's client ID, not your Android client ID.
-                                .setServerClientId(getString(R.string.web_client_id))
-                                // Only show accounts previously used to sign in.
-                                .setFilterByAuthorizedAccounts(false)
-                                .build()
-                )
-                .setAutoSelectEnabled(true)
-                .build();
+        watch_btn = findViewById(R.id.watch_video_btn);
+        welcome = findViewById(R.id.welcome);
 
-        SignInButton signInButton = findViewById(R.id.signIn);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
-        signInButton.setOnClickListener(new View.OnClickListener() {
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if(account == null) {
+            // Send the user to login if they are not signed in
+            Intent login = new Intent(getApplicationContext(), Login.class);
+            startActivity(login);
+        } else {
+            String userName = account.getDisplayName();
+            welcome.setText("Welcome to Video Sharing, " + userName);
+        }
+
+        watch_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signInOnClick(view);
+                Intent login = new Intent(getApplicationContext(), Login.class);
+                startActivity(login);
             }
         });
 
-        signInRequestLauncher =
-                registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(),
-                        new ActivityResultCallback<ActivityResult>() {
-                            @Override
-                            public void onActivityResult(ActivityResult result) {
-                                if(result.getResultCode() == Activity.RESULT_OK){
-                                    // sign in successful
-                                    try {
-                                        SignInCredential credential = oneTapClient
-                                                .getSignInCredentialFromIntent(result.getData());
-                                        String idToken = credential.getGoogleIdToken();
-                                        if (idToken != null) {
-                                            // Got an ID token from Google to use for authentication
-                                            //Intent intent = new Intent(
-                                                  //  getApplicationContext(),
-                                                  //  YouTubePlayerActivtiy.class);
-                                            //intent.putExtra("CREDENTIAL", credential);
-                                           // startActivity(intent);
-                                        }
-                                    } catch (ApiException e) {
-                                        Log.d("API Exception", "issue with sign in");
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }
-                        });
     }
-
-    public void signInOnClick(View view) {
-        oneTapClient.beginSignIn(signInRequest)
-                .addOnSuccessListener(this, new OnSuccessListener<BeginSignInResult>() {
-                    @Override
-                    public void onSuccess(BeginSignInResult beginSignInResult) {
-                        IntentSenderRequest intentSenderRequest =
-                                new IntentSenderRequest.Builder(beginSignInResult.getPendingIntent().getIntentSender())
-                                        .build();
-                        signInRequestLauncher.launch(intentSenderRequest);
-                    }
-                })
-                .addOnFailureListener(this, new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("FAILURE", "sign in failed!");
-                        e.printStackTrace();
-                    }
-                });
-    }
-
 }
